5d614d8bc0a33698aea083efcb9ebcbe906a8523
jvesche20 <70725626+jvesche20@users.noreply.github.com>
2021-04-01 21:41:39 -0400

    Create deleteStatus.js

---
+++ middleware/deleteStatus.js
@@ -0,0 +1,9 @@
+const StatusCodes = require('http-status-codes');
+
+module.exports = (request, response, next) => {
+  if (request.method === 'DELETE') {
+    response.sendStatus(StatusCodes.METHOD_NOT_ALLOWED);
+  } else {
+    next();
+  }
+};
